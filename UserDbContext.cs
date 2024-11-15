89d36051c80476730bf3ca0a65b2a35e769fffc1
mendozaeiron <92189026+mendozaeiron@users.noreply.github.com>
2024-11-15 18:28:52 -0300

    Add files via upload
    Estructura general proyecto Tipo, para ver principios SOLID y conocimientos en C#, Angular, Typescript, Etc. Github al no permitir subir carpetas solo deja subir archivos al repositorio. La estructura del proyecto original se incorporará en un formato .zip en un enlace de drive.

---
+++ UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagmentAPI.Models;
+
+namespace UserManagmentAPI.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
